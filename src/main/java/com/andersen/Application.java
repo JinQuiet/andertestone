614cdafe7d49b25621794084f1d3e9be8683d708
JinQuiet <Quester82@gmail.com>
2021-12-01 23:44:22 +0300

    pom.xml updated for the Spring Boot. Application modified to be a Spring Boot application

---
+++ src/main/java/com/andersen/Application.java
@@ -0,0 +1,14 @@
+package com.andersen;
+/*
+Application.java is the entry point of the application
+*/
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
