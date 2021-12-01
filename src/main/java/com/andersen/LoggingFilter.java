79d9c8f5b36126205878398b7b28885d2f21b472
JinQuiet <Quester82@gmail.com>
2021-12-02 02:04:06 +0300

    Added LoggingFilter to time requests to pages. Added  Hello Java required by the task. Respective modifications

---
+++ src/main/java/com/andersen/LoggingFilter.java
@@ -0,0 +1,29 @@
+package com.andersen;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.filter.GenericFilterBean;
+
+public class LoggingFilter extends GenericFilterBean{
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+      HttpServletRequest request = (HttpServletRequest) req;
+      
+      long start = new Date().getTime();
+  
+      //You must call this method to get any answer from the server
+      chain.doFilter(req, res);
+  
+      long elapsed = new Date().getTime() - start;
+  
+      logger.debug("Request[uri=" + request.getRequestURI() + ", method=" + request.getMethod() + "] completed in " + elapsed + " ms");
+    }
+    
+}
